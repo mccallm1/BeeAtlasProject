b5148b27fca175c42cf3d8338bef8e38839234c4
Miles McCall <mccallm@oregonstate.edu>
2018-11-21 15:50:19 -0800

    testing scripts

--- src/backend/gen_random_data.py
+++ src/backend/gen_random_data.py
@@ -8,18 +8,8 @@ import os
 import sys
 import argparse
 import random
-#from random import uniform
-#from random import choice
-#from random import seed
-#from random import random
 
 #Functions
-#def gen_row():
-#    row_string =  str(uniform(20.0, 60.0)) + ","
-#    row_string = row_string + str(uniform(100.0, 140.0)) + ","
-#    row_string = row_string + str(choice(['Apis mellifera','Lasioglossum','Seladonia','Osmia']))
-#    return row_string
-
 def write_csv(num_rows):
     print("Writing randomized entries to output csv...")
 
