9e53c39a48377fb4f2f19682067bb99f8380dee8
Miles McCall <mccallm@oregonstate.edu>
2018-12-04 19:13:54 -0800

    edit plotter

--- src/web/main/dev/point_plotter.js
+++ src/web/main/dev/point_plotter.js
@@ -1,8 +1,7 @@
 var points = svg.append('g');
 
 points.selectAll('path')
-  .data('data/test_results_geo_small.json'.features)
-  //.enter()
+  .data('data/test_results_geo_small.json'.features).enter()
   .append('path')
   .attr('fill', '#900')
   .attr('stroke', '#999')
