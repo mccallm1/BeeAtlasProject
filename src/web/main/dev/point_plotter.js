a5ef4eaa1e3efcf0150f87866d24ea48c33aa603
Miles McCall <mccallm@oregonstate.edu>
2018-12-04 17:28:57 -0800

    converted results to json; created points js;

---
+++ src/web/main/dev/point_plotter.js
@@ -0,0 +1,9 @@
+var points = svg.append('g');
+
+points.selectAll('path')
+  .data('data/test_results.json'.features)
+  .enter()
+  .append('path')
+  .attr('fill', '#900')
+  .attr('stroke', '#999')
+  .attr('d', geoPath);
