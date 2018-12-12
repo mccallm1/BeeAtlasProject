6fa82603abb85d08b915dbf1dbf26c2379f98b09
Miles McCall <mccallm@oregonstate.edu>
2018-12-12 10:14:56 -0800

    remaining changes to upload

--- src/backend/gen_random_data.py
+++ src/backend/gen_random_data.py
@@ -9,10 +9,9 @@ import sys
 import random
 
 #Functions
-def write_csv(num_rows):
+def write_csv(num_rows, file_name):
     print("Writing randomized entries to output csv...")
-
-    f = open("results/test_results.csv", "a")
+    f = open(file_name, "a")
     for i in range(num_rows):
         row_string = str(i) + "," # ID number
         row_string = row_string + str(random.randint(20,60)) + "," # Latitude
@@ -20,13 +19,14 @@ def write_csv(num_rows):
         row_string = row_string + str(random.choice(['Apis mellifera','Lasioglossum','Seladonia','Osmia'])) # Species Name
         row_string = row_string + "\n"
         f.write(row_string)
-
     print("\t--> Done.")
 
 def main():
-    print 'Argument List:', str(sys.argv)
+    # Example Command-line input:
+        # python gen_random_number.py 500 500rows.csv
     num_rows = int(sys.argv[1])
-    write_csv(num_rows)
+    file_name = "results/" + str(sys.argv[2])
+    write_csv(num_rows, file_name)
 
 if __name__ == '__main__':
     main()
